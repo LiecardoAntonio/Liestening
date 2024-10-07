1b6e43d99ec45f45a0fcfc354cf67bf95a1b7c99
LiecardoAntonio <liecardoantonio24@gmail.com>
2024-10-07 21:52:46 +0700

    progress10

--- script.js
+++ script.js
@@ -191,7 +191,15 @@ const playPreviousSong = () => {
 previousButton.addEventListener("click", playPreviousSong);
 
 //Display the current song title and artist in the player display
-const setPlayerDisplay = () => {};
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById('player-song-title');
+  const songArtist = document.getElementById('player-song-artist');
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+  // setting the text content based by the current title & artist
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
+};
 
 //highlight the currently playing song
 const highlightCurrentSong = () => {
