9db53860b13c9ad15deb9a37629ce1107de38687
LiecardoAntonio <liecardoantonio24@gmail.com>
2024-10-09 12:51:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -253,6 +253,11 @@ const highlightCurrentSong = () => {
   }
 };
 
+// The only issue now is that the next song does not automatically play when the currently playing song ends. To fix that, you can set up an event listener which will detect when the currently playing song ends. The "ended" event listener is appropriate for this. It is fired when the playback of a media reaches the end. Add an event listener to the audio element which listens for the "ended" event. Pass in a callback using arrow syntax with empty curly braces.
+audio.addEventListener('ended', () => {
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists  = userData.songs.length-1 > currentSongIndex ? true : false;
+});
 
 const renderSongs = (array) => {
   const songsHTML = array.map((song) => { ////array.map function takes another function as its parameter
